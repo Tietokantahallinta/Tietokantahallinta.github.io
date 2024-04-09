bb6872ee0a5ab0a302beff9725a895dbfc67d426
SauliIsonikkila <sauli.isonikkila@gmail.com>
2024-04-09 19:19:38 +0300

    Vko 02

--- luennot/viikko02/Tehtava_3_Vastaus.sql
+++ luennot/viikko02/Tehtava_3_Vastaus.sql
@@ -1,1 +1,17 @@
-create index ix_KerhonJasenet on KerhonJasenet(KerhoNro, JasenID);
+Perustelut:
+Vielä ei tiedetä tämän tietokannan käyttöä. Käyttäjiä on tosin 
+vähän. 6 henkilökuntaa ja 20 asiakasta. Se on vähän. 
+Mutta luodaan varovaisesti ehkä todennäköisimmin eniten liitoksia
+käyttävään tauluun yksi ulkoinen indeksi. Kaikki FX kentät ovat siis
+potenttiaalisia indeksointi kohteita.
+
+create index IX_KerhonJasenet_FKt 
+on KerhonJasenet(KerhoNro, JasenID);
+
+create index IX_Jasen_Sukunimi_Etunimi on Jasen(Sukunimi, Etunimi);
+
+Tämän jälkeen ryhdytään seuraamaan tietokannan taulujen todellista käyttöä.
+Sitä on mahdollista monitoroida. Näillä tunneilla tulee asiasta tarkemmin
+seuraavilla tunneilla.
+
+Kun todellista käyttötilastoa saadaan, voidaan lisätä lisää indeksejä. Tai poistaa niitä.
