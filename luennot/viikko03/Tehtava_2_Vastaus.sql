0814f0e0b27e952ddc613c9f9e67c9b9517fbe7a
SauliIsonikkila <sauli.isonikkila@gmail.com>
2024-04-16 15:47:54 +0300

    Vko 03

--- luennot/viikko03/Tehtava_2_Vastaus.sql
+++ luennot/viikko03/Tehtava_2_Vastaus.sql
@@ -1,11 +1,11 @@
 
-revoke select, insert, update, delete, alter on Jasen to DataAdmin cascade;
-revoke select, insert, update, delete, alter on Osoite to DataAdmin cascade;
-revoke select, insert, update, delete, alter on KerhonJasenet to DataAdmin cascade;
-revoke select, insert, update, delete, alter on Postinumero to DataAdmin cascade;
-revoke select, insert, update, delete, alter on Kerho to DataAdmin cascade;
-revoke select, insert, update, delete, alter on Yhdistys to DataAdmin cascade;
-revoke select, insert, update, delete, alter on Tyontekija to DataAdmin cascade;
+revoke select, insert, update, delete, alter on Jasen to paakayttaja cascade;
+revoke select, insert, update, delete, alter on Osoite to paakayttaja cascade;
+revoke select, insert, update, delete, alter on KerhonJasenet to paakayttaja cascade;
+revoke select, insert, update, delete, alter on Postinumero to paakayttaja cascade;
+revoke select, insert, update, delete, alter on Kerho to paakayttaja cascade;
+revoke select, insert, update, delete, alter on Yhdistys to paakayttaja cascade;
+revoke select, insert, update, delete, alter on Tyontekija to paakayttaja cascade;
 
 
 revoke select, insert, update, delete on Jasen to kayttaja1;
@@ -25,18 +25,36 @@ revoke select on Postinumero to johtaja cascade;
 revoke select on Kerho to johtaja cascade;
 revoke select on Yhdistys to johtaja cascade;
 
-create role paakayttaja authorization DataAdmin;
-grant select, insert, update, delete, alter on Jasen to paakayttaja with grant option;
-grant select, insert, update, delete, alter on Osoite to paakayttaja with grant option;
-grant select, insert, update, delete, alter on KerhonJasenet to paakayttaja with grant option;
-grant select, insert, update, delete, alter on Postinumero to paakayttaja with grant option;
-grant select, insert, update, delete, alter on Kerho to paakayttaja with grant option;
-grant select, insert, update, delete, alter on Yhdistys to paakayttaja with grant option;
-grant select, insert, update, delete, alter on Tyontekija to paakayttaja with grant option;
-sp_addrolemember 'paakayttaja', 'johtaja';
+create role DataAdmin authorization paakayttaja;
+grant select, insert, update, delete, alter on Jasen to DataAdmin with grant option;
+grant select, insert, update, delete, alter on Osoite to DataAdmin with grant option;
+grant select, insert, update, delete, alter on KerhonJasenet to DataAdmin with grant option;
+grant select, insert, update, delete, alter on Postinumero to DataAdmin with grant option;
+grant select, insert, update, delete, alter on Kerho to DataAdmin with grant option;
+grant select, insert, update, delete, alter on Yhdistys to DataAdmin with grant option;
+grant select, insert, update, delete, alter on Tyontekija to DataAdmin with grant option;
+EXEC sp_addrolemember 'DataAdmin', 'johtaja';
+EXEC sp_addrolemember 'DataAdmin', 'paakayttaja';
 
+create role KerhonOhjaaja authorization paakayttaja;
+grant select, insert, update, delete on Jasen to KerhonOhjaaja;
+grant select, insert, update, delete on Osoite to KerhonOhjaaja;
+grant select, insert, update, delete on KerhonJasenet to KerhonOhjaaja;
+grant select, insert, update, delete on Postinumero to KerhonOhjaaja;
+grant select, insert, update, delete on Kerho to KerhonOhjaaja;
+grant select on Yhdistys to KerhonOhjaaja;
+grant select,  update on Tyontekija to KerhonOhjaaja;
+EXEC sp_addrolemember 'KerhonOhjaaja', 'Ohjaaja';
 
-
+create role Jasen authorization paakayttaja;
+grant select, update on Jasen to Jasen;
+grant select, update on Osoite to Jasen;
+grant select, insert on KerhonJasenet to Jasen;
+grant select on Postinumero to Jasen;
+grant select on Kerho to Jasen;
+grant select on Yhdistys to Jasen;
+grant select on Tyontekija to Jasen;
+EXEC sp_addrolemember 'Jasen', 'kayttaja1';
 
 
 
