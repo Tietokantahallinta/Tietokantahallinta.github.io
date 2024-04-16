a2c506f4792fc6a02b5b59976ccb16eab1bcb119
SauliIsonikkila <sauli.isonikkila@gmail.com>
2024-04-16 16:39:14 +0300

    Vko 04

---
+++ luennot/viikko04/Pankki.sql
@@ -0,0 +1,44 @@
+create table Tilit (
+	tilinro int not null primary key,
+	saldo decimal(10,2) not null
+);
+
+Aloitus tilanne:
+delete from tilit;
+insert into Tilit (tilinro, saldo) values (710100, 2000);
+insert into Tilit (tilinro, saldo) values (910101, 200000);
+
+select * from tilit;
+
+User 2: Milla suorittaa operaattori maksun 40 e
+Ilman transaktioiden hallintaa
+update Tilit set saldo = saldo - 40 where tilinro = 710100;
+update Tilit set saldo = saldo + 40 where tilinro = 910101;
+
+User 2: Milla maksaa operaattori maksunsa, 40 e.
+begin transaction
+update Tilit set saldo = saldo - 40 where tilinro = 710100;
+update Tilit set saldo = saldo + 40 where tilinro = 910101;
+commit
+
+select * from tilit;
+
+(rollback)
+
+User 1: Pankin kontrollori
+select * from Tilit where tilinro = 910101;
+
+User 2: Milla maksaa operaattori maksunsa, 40 e.
+Ajetaan ensimmäiseksi
+begin transaction
+update Tilit set saldo = saldo - 40 where tilinro = 710100;
+update Tilit set saldo = saldo + 40 where tilinro = 910101;
+
+Ajeteaan kolmanneksi
+select * from tilit;
+
+Ajetaan neljänneksi
+commit
+select * from tilit;
+
+
---
+++ luennot/viikko04/Pankki_User1.sql
@@ -0,0 +1,15 @@
+User 1: Pankin kontrollori
+Ajetaan toisena
+select * from Tilit where tilinro = 910101;
+
+select * from tilit;
+
+
+Ajetaan viidenneksi:
+select * from tilit;
+
+
+
+
+
+
